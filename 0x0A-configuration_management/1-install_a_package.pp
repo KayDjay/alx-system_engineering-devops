4c5221de1fc6cef9ef9335a1ada47584518f08e5
root <Khadijatrasaq@gmail.com>
2023-11-24 14:32:36 -0800

    Create and Install

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# This is to instal flask using puppet
+
+package { 'pip':
+  ensure  => 'installed',
+}
+
+exec { 'flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin/',
+  require => Package['pip'],
+}
