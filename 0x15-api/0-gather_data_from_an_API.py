ef668733a93a216a869f90d22f0ce3a01a68a329
root <Khadijatrasaq@gmail.com>
2024-01-02 15:44:42 -0800

    getdata

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python3
+
 """ Get TODO list from REST API """
 
-import requests
+
+from requests import get
 from sys import argv
 
-if __name__ == '__main__':
-    employeeId = argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
 
-    response = requests.get(url)
-    employee_Name = response.json().get('name')
+def getdata(data=[], done=False):
+    """
+    print only completed tasks or return the count
+    of completed and total tasks,
+    """
+    if done is True:
+        for a in data:
+            if a.get("completed") is True:
+                print("\t ", a.get("title"))
+    else:
+        done = 0
+        total = len(data)
+        if not data:
+            return (done, total)
+        for a in data:
+            if a.get("completed") is True:
+                done += 1
+        return (done, total)
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-    done = 0
-    done_tasks = []
 
-    for task in tasks:
-        if task.get("completed") is True:
-            done += 1
-            done_tasks.append(task.get("title"))
-    print(f"Employee {employee_Name} is done with tasks({done}/{len(tasks)}):")
-    for i in done_tasks:
-        print("\t ", i)
+if __name__ == "__main__":
+    if len(argv) >= 2:
+        user_id = get(f"https://jsonplaceholder.typicode.com/users"
+                      f"/{argv[1]}")
+        todos = get(f"https://jsonplaceholder.typicode.com/users"
+                    f"/{argv[1]}/todos")
+        if user_id.status_code == 200:
+            EMPLOYEE_NAME = user_id.json().get("name")
+            data = todos.json()
+            NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS = getdata(data)
+            print(f"Employee {EMPLOYEE_NAME} is done with tasks("
+                  f"{NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+            getdata(data, True)
