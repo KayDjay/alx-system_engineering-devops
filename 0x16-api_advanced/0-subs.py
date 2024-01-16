77c1a965508673e8d4861f17773f311188d0a4a1
kaydjay <khadijatrasaq@gmail.com>
2024-01-17 00:24:05 +0100

    Refactor code for improved performance

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Kaydee"}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,21 @@
+import requests
+
+def recurse(subreddit, hot_list=[]):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Kaydee"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        children = data.get("data", {}).get("children", [])
+        for child in children:
+            title = child.get("data", {}).get("title")
+            hot_list.append(title)
+        
+        after = data.get("data", {}).get("after")
+        if after:
+            return recurse(subreddit, hot_list=hot_list)
+        else:
+            return hot_list
+    else:
+        return None
